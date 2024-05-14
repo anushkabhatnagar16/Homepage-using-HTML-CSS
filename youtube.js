9148d5f8128aa60e9a1c1bb79ea71043b5d717e2
ishitasingla10 <ishitasingla100@gmail.com>
2024-05-14 22:49:28 +0530

    css is modified

--- youtube.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('menu-icon').addEventListener('click', function() {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.style.display = sidebar.style.display === 'block' ? 'none' : 'block';
-});
